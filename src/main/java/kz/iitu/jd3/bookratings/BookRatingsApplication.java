7f71d46570fb1549f27b6ebae03f7e705b06fa61
Zhanbolat Seitkulov <zhanbolatseitkulov@gmail.com>
2019-09-30 17:02:35 +0600

    registering eureka client

--- src/main/java/kz/iitu/jd3/bookratings/BookRatingsApplication.java
+++ src/main/java/kz/iitu/jd3/bookratings/BookRatingsApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.jd3.bookratings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookRatingsApplication {
 
 	public static void main(String[] args) {
